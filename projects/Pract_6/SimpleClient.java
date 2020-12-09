c47c6be89fcb1432a4cc54b2125c257c1112ae95
Vova <45471242+BoyhenkoX44@users.noreply.github.com>
2020-12-09 21:40:25 +0200

    added pract6

---
+++ projects/Pract_6/SimpleClient.java
@@ -0,0 +1,30 @@
+﻿package Pract_6;
+import java.io.*;
+import java.net.*;
+
+public class SimpleClient {
+    public static void main(String[] args) {
+        Socket s = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        String serverAddr = "localhost";
+        int port = 8030;
+        try {
+            s = new Socket(serverAddr,port);
+            isr = new InputStreamReader(s.getInputStream());
+            br = new BufferedReader(isr);
+            String msg = br.readLine();
+            System.out.println("Повідомлення: " + msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                br.close();
+                isr.close();
+                s.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+}
---
+++ projects/Pract_6/SimpleServer.java
@@ -0,0 +1,35 @@
+﻿package Pract_6;
+import java.io.*;
+import java.net.*;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        Socket s = null;
+        ServerSocket servSocket = null;
+        PrintStream ps = null;
+        int port = 8030;
+        try {
+            servSocket = new ServerSocket(port);
+            s = servSocket.accept();
+            System.out.print("Підключився клієнт [");
+            System.out.println(s.getInetAddress()+"]");
+            ps = new PrintStream(s.getOutputStream());
+            ps.println("Привіт");
+            ps.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            ps.close();
+            try {
+                s.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            try {
+                servSocket.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+}
